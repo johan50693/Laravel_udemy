e2a5b23a5425de7988a256a9e9bc405265d455d9
Albert <johan50693@gmail.com>
2020-02-03 22:05:36 -0400

    ngClass

--- src/app/directiva/directiva.component.ts
+++ src/app/directiva/directiva.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 interface Producto{
-	
+  nombre:string;
+  stock:number;
+  fabricante:string;
+  fechaVence:Date;
+  esImportante: Boolean;
+  textoGrande: Boolean;
 }
 
 @Component({
@@ -13,7 +18,34 @@ export class DirectivaComponent implements OnInit {
 
 	cargando:boolean =true;
 	nombres: Array<string>= ["Juan","Pedro","María","Juana","Pablo","Marta"];
-
+  pestana: string='';
+  mostrarCuadrado : boolean=false;
+  productos: Array<Producto>=[
+    {
+      nombre: 'Laptop',
+      stock:20,
+      fabricante: 'HP',
+      fechaVence: new Date('04/19/2020'),
+      esImportante:true,
+      textoGrande:false,
+    },
+    {
+      nombre: 'Redmi 7',
+      stock:10,
+      fabricante: 'Xiaomi',
+      fechaVence: new Date('01/19/2020'),
+      esImportante:false,
+      textoGrande:true,
+    },
+    {
+      nombre: 'Mouse',
+      stock:70,
+      fabricante: 'Genius',
+      fechaVence: new Date('10/05/2025'),
+      esImportante:true,
+      textoGrande:false,
+    },
+  ];
 
   constructor() { }
 
@@ -26,6 +58,12 @@ export class DirectivaComponent implements OnInit {
   alterLoading(){
   	this.cargando=!this.cargando;
   }
+  cambiarPestana(pestana: string){
+    this.pestana=pestana;
+  }
 
+  alterFondo(){
+  	this.mostrarCuadrado=!this.mostrarCuadrado;
+  }
 
 }

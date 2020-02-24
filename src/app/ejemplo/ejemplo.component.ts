0fb44043e2c8f0d3a69200825e5a00a96e98482b
Albert <johan50693@gmail.com>
2020-02-24 11:38:01 -0400

    Ejemplos 1 y 2 para el usode formularios con ngmodel y ciclo ngFor

--- src/app/ejemplo/ejemplo.component.ts
+++ src/app/ejemplo/ejemplo.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+class Alumno{
+  nombre:string;
+  apellido:string;
+  excelencia:Boolean;
+
+  constructor(){
+    this.nombre=this.nombre;
+    this.apellido=this.apellido;
+    this.excelencia=this.excelencia;
+  }
+}
+
 @Component({
   selector: 'app-ejemplo',
   templateUrl: './ejemplo.component.html',
@@ -7,9 +19,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EjemploComponent implements OnInit {
 
+  alumno :Alumno= new Alumno();
+  listadoAlumnos :Array<Alumno> = new Array<Alumno>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  agregar(){
+    this.listadoAlumnos.push({
+      nombre: this.alumno.nombre,
+      apellido: this.alumno.apellido,
+      excelencia: this.alumno.excelencia
+    });
+    this.alumno.nombre='';
+    this.alumno.apellido='';
+    this.alumno.excelencia=false;
+    console.log(this.listadoAlumnos);
   }
 
 }
